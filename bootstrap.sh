a2fa20e74404c4e922d6290525ef7a288abbc9c5
Jo-Ries Canino <canino_jories@hotmail.com>
2016-07-02 12:59:38 +0800

    adding args in bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -28,18 +28,18 @@ usage() {
     \e[36m\e[1mdotfiles\e[0m \e[96m--[parameters]\e[0m
 
     \e[36m\e[1mParameters:\e[0m\e[36m
-        \e[36m- casks      : \e[0m\e[96m installs default native apps via brew-cask\e[0m
-        \e[36m- fonts      : \e[0m\e[96m installs default fonts declared from scripts/fonts\e[0m
-        \e[36m- git        : \e[0m\e[96m setup for glolbal git config and setup for Github & Bitbucket SSH Keys\e[0m
-        \e[36m- nodepkgs   : \e[0m\e[96m installs npm packages for the default node version\e[0m
-        \e[36m- osxdefault : \e[0m\e[96m default setup for osx (for OS X >= 10.8)\e[0m
-        \e[36m- rubygems   : \e[0m\e[96m installs gems for web development\e[0m
-        \e[36m- terminal   : \e[0m\e[96m setup terminal to use zshell and oh-my-zshell plugin\e[0m\e[0m
+        \e[36m- brew        : \e[0m\e[96m brew services and and cask\e[0m
+        \e[36m- brew-package: \e[0m\e[96m homebew formulas\e[0m
+        \e[36m- terminal    : \e[0m\e[96m setup terminal to use zshell and oh-my-zshell plugin\e[0m\e[0m
+        \e[36m- osx         : \e[0m\e[96m default setup for osx (for OS X >= 10.8)\e[0m
+        \e[36m- casks       : \e[0m\e[96m installs default native apps via brew-cask\e[0m
+        \e[36m- fonts       : \e[0m\e[96m installs default fonts declared from scripts/fonts\e[0m
+        \e[36m- git         : \e[0m\e[96m setup for glolbal git config and setup for Github & Bitbucket SSH Keys\e[0m
+        \e[36m- npm         : \e[0m\e[96m installs npm packages for the default node version\e[0m
   "
 }
 
 help() {
-  name
   usage
 }
 
@@ -50,17 +50,17 @@ fi
 case "$@" in
   # Install first the terminal because it will used for the cask
   # HOMEBREW_CASK_OPTS="--appdir=/Applications"
-  ('--terminal')
-    exec "${HOME}/.dotfiles/bin/terminal"; break;;
-  ('--osx')
-    exec "${HOME}/.dotfiles/bin/osx"; break;;
   ('--brew')
     exec "${HOME}/.dotfiles/bin/brew"; break;;
   ('--brew-package')
     dir="/opt/dotfiles"
     sudo mkdir -p ${dir}
     sudo chown ${USER} ${dir}
-    exec "${HOME}/Desktop/dotfiles/bin/brew-package"; break;;
+    exec "${HOME}/.dotfiles/bin/brew-package"; break;;
+  ('--terminal')
+    exec "${HOME}/.dotfiles/bin/terminal"; break;;
+  ('--osx')
+    exec "${HOME}/.dotfiles/bin/osx"; break;;
   ('--casks')
     exec "${HOME}/.dotfiles/bin/casks"; break;;
   ('--git')
